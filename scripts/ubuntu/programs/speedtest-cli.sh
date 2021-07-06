dbcdba418386f1b102b5d81eb8e5a3836c0418df
Craig Treasure <craigktreasure@outlook.com>
2021-07-06 16:30:18 -0700

    Fixed speedtest install

--- scripts/ubuntu/programs/speedtest-cli.sh
+++ scripts/ubuntu/programs/speedtest-cli.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
 # https://www.speedtest.net/apps/cli
-sudo apt-get install -y gnupg1 apt-transport-https dirmngr
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-echo "deb https://ookla.bintray.com/debian generic main" | sudo tee /etc/apt/sources.list.d/speedtest.list
-sudo apt-get update
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt-get install -y speedtest
