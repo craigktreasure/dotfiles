967a1d1d320a3655457ff0a450b37efe50897b33
Craig Treasure <crtreasu@microsoft.com>
2022-04-07 18:43:10 -0700

    Install latest PSReadLine

--- scripts/powershell/configurations/modules.ps1
+++ scripts/powershell/configurations/modules.ps1
@@ -4,8 +4,10 @@ $ErrorActionPreference = "Stop"
 
 Write-Host 'Installing PowerShell modules...' -ForegroundColor Magenta
 
+Install-Module PSReadLine -Force
+
 if (-not (Get-InstalledModule -Name posh-git -ErrorAction SilentlyContinue)) {
-    PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+    PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 } else {
     Write-Host 'posh-git already installed.' -ForegroundColor Yellow
 }
