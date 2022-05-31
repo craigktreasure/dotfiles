78ec34f9593f0322704f7145eee07ba956e83910
Craig Treasure <craigktreasure@outlook.com>
2022-05-31 10:50:44 -0700

    Install the .NET 6 Azure Artifact Credential Provider

--- scripts/powershell/common/programs/azure-artifact-credential-provider.ps1
+++ scripts/powershell/common/programs/azure-artifact-credential-provider.ps1
@@ -5,4 +5,4 @@ $ErrorActionPreference = "Stop"
 Write-Host 'Installing Azure Artifact Credential Provider...'
 
 # https://github.com/microsoft/artifacts-credprovider
-Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-artifacts-credprovider.ps1) }"
+Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-artifacts-credprovider.ps1) } -InstallNet6"
