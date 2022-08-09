cf37083ab52579bc7808e0ae6cff9c54c8a6fa8e
Craig Treasure <crtreasu@microsoft.com>
2022-08-09 16:34:36 -0700

    Fix Visual Studio installation

--- scripts/powershell/windows/programs/visual-studio.ps1
+++ scripts/powershell/windows/programs/visual-studio.ps1
@@ -2,10 +2,16 @@
 
 $ErrorActionPreference = "Stop"
 
-Write-Host 'Installing Visual Studio...'
+. (Join-Path $PSScriptRoot '..' 'winget-helpers.ps1')
 
+Write-Host 'Installing Visual Studio...' -ForegroundColor Magenta
+
+if (Test-WingetInstalledByIdLike 'Microsoft.VisualStudio.2022.') {
+    Write-Host 'Already installed.'
+    return
+}
 
 # We override the arguments to add additional workloads and to wait using `--wait`
 # so that the install command can wait for the installer to complete.
 # See https://github.com/microsoft/winget-cli/issues/670.
-winget install --id=Microsoft.VisualStudio.Community --exact --override "--productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.16.Release --channelUri https://aka.ms/vs/16/release/channel --add Microsoft.VisualStudio.Workload.Universal --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --includeRecommended --wait --norestart --passive"
+winget install --id=Microsoft.VisualStudio.2022.Enterprise --exact --override "--productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.16.Release --channelUri https://aka.ms/vs/16/release/channel --add Microsoft.VisualStudio.Workload.Universal --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --includeRecommended --wait --norestart --passive"
--- scripts/powershell/windows/winget-helpers.ps1
+++ scripts/powershell/windows/winget-helpers.ps1
@@ -4,6 +4,12 @@ function Test-WingetInstalledById([string] $Id) {
     return $LASTEXITCODE -eq 0
 }
 
+function Test-WingetInstalledByIdLike([string] $Id) {
+    & winget list --id $id
+
+    return $LASTEXITCODE -eq 0
+}
+
 function Test-WingetInstalledByName([string] $name) {
     & winget list --name $name --exact
 
