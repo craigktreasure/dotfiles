8d57ed6278481606759e5c055f95fe406c9af777
Craig Treasure <crtreasu@microsoft.com>
2022-08-09 16:47:01 -0700

    Fix Windows Terminal installation

--- scripts/powershell/windows/programs/windows-terminal.ps1
+++ scripts/powershell/windows/programs/windows-terminal.ps1
@@ -2,6 +2,17 @@
 
 $ErrorActionPreference = "Stop"
 
-Write-Host 'Installing Windows Terminal...'
+. (Join-Path $PSScriptRoot '..' 'winget-helpers.ps1')
 
-winget install --id=Microsoft.WindowsTerminal --exact --source msstore
+Write-Host 'Installing Windows Terminal...' -ForegroundColor Magenta
+
+# The MS Store package id
+$wingetId = '9N0DX20HK701'
+
+if (Test-WingetInstalledById $wingetId -or Test-WingetInstalledById 'Microsoft.WindowsTerminal') {
+    Write-Host 'Already installed.'
+    return
+}
+
+
+winget install --id=$wingetId --exact --source msstore
