b3849abe0013355021e7c962aedd3d248c7a2b1d
Craig Treasure <craigktreasure@outlook.com>
2022-12-16 21:31:44 -0800

    Make .NET installation interactive on Windows to prevent unexpected reboots

--- scripts/powershell/windows/programs/dotnet.ps1
+++ scripts/powershell/windows/programs/dotnet.ps1
@@ -9,7 +9,7 @@ Write-Host 'Installing .NET 6 SDK...' -ForegroundColor Magenta
 $wingetId = 'Microsoft.DotNet.SDK.6'
 
 if (-not (Test-WingetInstalledById $wingetId)) {
-    winget install --id=$wingetId --exact
+    winget install --id=$wingetId --exact  --interactive
 }
 else {
     Write-Host 'Already installed.'
@@ -20,7 +20,7 @@ Write-Host 'Installing .NET 7 SDK...' -ForegroundColor Magenta
 $wingetId = 'Microsoft.DotNet.SDK.7'
 
 if (Test-WingetInstalledById $wingetId) {
-    winget install --id=$wingetId --exact
+    winget install --id=$wingetId --exact --interactive
 }
 else {
     Write-Host 'Already installed.'
