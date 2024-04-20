38528a8a46b67d87533d20db3be7ed8294016f54
Craig Treasure <craigktreasure@outlook.com>
2024-04-20 14:27:40 -0700

    Remove .NET 7 installation

--- scripts/powershell/windows/programs/dotnet.ps1
+++ scripts/powershell/windows/programs/dotnet.ps1
@@ -7,8 +7,5 @@ $ErrorActionPreference = "Stop"
 Write-Host 'Installing .NET 6 SDK...' -ForegroundColor Magenta
 Install-WingetPackageById 'Microsoft.DotNet.SDK.6'
 
-Write-Host 'Installing .NET 7 SDK...' -ForegroundColor Magenta
-Install-WingetPackageById 'Microsoft.DotNet.SDK.7'
-
 Write-Host 'Installing .NET 8 SDK...' -ForegroundColor Magenta
 Install-WingetPackageById 'Microsoft.DotNet.SDK.8'
