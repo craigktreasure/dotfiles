11f1bda8ad2445e4331d2803f968caa3f598c5f6
Craig Treasure <craigktreasure@outlook.com>
2024-12-30 11:31:00 -0800

    .NET on Windows: Replace .NET 6 with 9

--- scripts/powershell/windows/programs/dotnet.ps1
+++ scripts/powershell/windows/programs/dotnet.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = "Stop"
 
 . (Join-Path $PSScriptRoot '..' 'winget-helpers.ps1')
 
-Write-Host 'Installing .NET 6 SDK...' -ForegroundColor Magenta
-Install-WingetPackageById 'Microsoft.DotNet.SDK.6'
-
 Write-Host 'Installing .NET 8 SDK...' -ForegroundColor Magenta
 Install-WingetPackageById 'Microsoft.DotNet.SDK.8'
+
+Write-Host 'Installing .NET 9 SDK...' -ForegroundColor Magenta
+Install-WingetPackageById 'Microsoft.DotNet.SDK.9'
