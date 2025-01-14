5dcce57346eca21366ae7b5bdae81ff2526207b5
Craig Treasure <crtreasu@microsoft.com>
2025-01-14 12:03:25 -0800

    Patch dotnet-suggest runtime configuration

--- scripts/powershell/common/programs/dotnet-tools.ps1
+++ scripts/powershell/common/programs/dotnet-tools.ps1
@@ -8,3 +8,12 @@ dotnet tool install -g dotnet-outdated-tool
 dotnet tool install -g nbgv
 dotnet tool install -g slnup
 dotnet tool install -g dotnet-suggest
+
+# Fix dotnet-suggest to run on newer runtime versions
+$dotnetSuggestRuntimeConfigPath = Join-Path $env:HOME '.dotnet\tools\.store\dotnet-suggest\1.1.415701\dotnet-suggest\1.1.415701\tools\net7.0\any\dotnet-suggest.runtimeconfig.json'
+if (Test-Path $dotnetSuggestRuntimeConfigPath) {
+    Write-Host 'Patching dotnet-suggest to run on newer runtime versions...'
+    $json = Get-Content $dotnetSuggestRuntimeConfigPath | ConvertFrom-Json
+    $json.runtimeOptions | Add-Member -MemberType NoteProperty -Name 'rollForward' -Value 'Major'
+    Set-Content -Value (ConvertTo-Json $json -Depth 50) -Path $dotnetSuggestRuntimeConfigPath
+}
