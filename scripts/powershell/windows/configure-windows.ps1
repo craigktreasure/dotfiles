59f0e551dd41b1239c56e6f9566d2023ff066eac
Craig Treasure <craigktreasure@outlook.com>
2025-03-31 17:47:13 -0700

    Make enabling LogPaths less intrusive if it's already done

--- scripts/powershell/windows/configure-windows.ps1
+++ scripts/powershell/windows/configure-windows.ps1
@@ -4,5 +4,24 @@ $ErrorActionPreference = "Stop"
 
 Write-Host 'Configuring Windows...'
 
-Write-Host 'Enabling LongPaths support in Windows'
-sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+function EnableLongPathsSupport {
+    $registryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem'
+    $propertyName = 'LongPathsEnabled'
+
+    if (Test-Path $registryPath) {
+        $currentValue = Get-ItemProperty -Path $registryPath -Name $propertyName -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty $propertyName -ErrorAction SilentlyContinue
+        if ($currentValue -ne 1) {
+            Write-Host 'Enabling LongPaths support in Windows'
+
+            sudo Set-ItemProperty -Path $registryPath -Name $propertyName -Value 1
+            Write-Host "Enabled."
+        } else {
+            Write-Host "LongPaths support already enabled."
+        }
+    } else {
+        throw "Registry path $registryPath does not exist."
+    }
+}
+
+EnableLongPathsSupport
